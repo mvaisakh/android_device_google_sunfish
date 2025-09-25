367599d7eadd6f95026ba5f44d1be4c3f8c299d0
Vaisakh Murali <mvaisakh@statixos.com>
2025-09-25 12:30:10 +0200

    sunfish: include pixel launcher

--- statix_sunfish.mk
+++ statix_sunfish.mk
@@ -27,4 +27,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=google/sunfish/sunfish:13/TQ3A.230805.001.S1/10786265:user/release-keys \
     DeviceProduct=sunfish
 
+# Include Pixel Launcher
+INCLUDE_PIXEL_LAUNCHER := true
+
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
