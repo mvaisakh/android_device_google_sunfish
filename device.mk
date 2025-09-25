e6ed5ea91c513bc330052e994eb3528297f81dac
Vaisakh Murali <mvaisakh@statixos.com>
2025-09-25 12:29:04 +0200

    sunfish: conditionally include misc_writer

--- device.mk
+++ device.mk
@@ -755,7 +755,7 @@ endif
 include hardware/google/pixel/common/pixel-common-device.mk
 
 # gs-common
-include device/google/gs-common/misc_writer/misc_writer.mk
+-include device/google/gs-common/misc_writer/misc_writer.mk
 
 # Citadel
 include hardware/google/pixel/citadel/citadel.mk
