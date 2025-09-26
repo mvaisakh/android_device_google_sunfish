5eec73fff4537ec3d13a685ef590f0815748dc63
Vaisakh Murali <mvaisakh@statixos.com>
2025-09-26 05:20:09 +0200

    sunfish: Drop speed profiling
    This is done via our own source
    
    Change-Id: Ic7830463da2d15b4aa20a1d1679ba43b72d95fe0

--- device.mk
+++ device.mk
@@ -544,14 +544,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.sys.modem.diag.mdlog_br_num=5
 
-# Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
-
-# Compile SystemUI on device with `speed`.
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed
-
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
 
